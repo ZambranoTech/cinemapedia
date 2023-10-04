60aa6b94ffacad588faa9c5d9526df6e08056f26
Javier García <a2027garciaj@gmail.com>
2023-10-04 16:41:50 +0200

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.blue
+  );
+
+}
---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MovieDatasource {
+
+
+  Future<List<Movie>> getNowPlaying({int page = 1});
+
+
+}
