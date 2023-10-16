87b17e985b2945df28fdf30c0ba8f1373b529462
Javier García <a2027garciaj@gmail.com>
2023-10-16 21:03:08 +0200

    fin de la aplicacion

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,4 @@
 import 'package:animate_do/animate_do.dart';
-import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
